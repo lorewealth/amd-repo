16ed85079476cde195764f936aa72c757b808590
Maksym Yatsko <lorewealth@gmail.com>
2026-07-13 21:55:02 +0300

    Am indeplinit Tema 1 si Tema 2 pentru day 10 + cerintele aditionale

---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+import { RouterLink } from 'vue-router';
+
+</script>
+
+<template>
+  <nav>
+    <RouterLink :to="{name: 'home'}" class="btn">Acasa</RouterLink>
+    <RouterLink :to="{name: 'runs'}" class="btn">Rularile</RouterLink>
+    <RouterLink :to="{name: 'about'}" class="btn">Despre</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+  .btn{
+    display: inline-block;
+    padding: 0.5em 1em;
+    background-color: #3a3a3a;
+    color: white;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 5px;
+  }
+  .btn:hover{
+    background-color: #4a4a4a;
+  }
+  .btn.router-link-active{
+    background-color: #d3d8d3
+  }
+</style>
---
+++ frontend/src/views/RunDetail.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { useRoute } from 'vue-router';
+const route = useRoute()
+</script>
+
+<template>
+  <p>Detalii pentru rularea #{{route.params.id}}</p>
+</template>
